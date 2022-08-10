9e5b33d4b568faea06813871ad2a686baf69d799
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-08-10 14:35:55 +0300

    edit test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,44 @@
 from app import app
 
 
-def base_get_test(url):
+def base_test(url, get=True):
     app.testing = True
     client = app.test_client()
-    response = client.get(url)
-    assert response.status_code == 200
+    if get:
+        response = client.get(url)
+        assert response.status_code == 200
+    else:
+        response = client.post(url)
+        assert response.status_code == 302
 
 
 def test_base_route():
-    base_get_test('/')
+    base_test('/')
 
 
 def test_home():
-    base_get_test('/home')
+    base_test('/home')
 
 
 def test_alternatives_input_get():
-    base_get_test('/alternatives_input')
+    base_test('/alternatives_input')
+
+
+def test_alternatives_input_post():
+    base_test('/alternatives_input', get=False)
 
 
 def test_categories_input_get():
-    base_get_test('/categories_input')
+    base_test('/categories_input')
+
+
+def test_categories_input_post():
+    base_test('/categories_input', get=False)
 
 
 def test_accordance_input_get():
-    base_get_test('/accordance_input')
+    base_test('/accordance_input')
 
+
+def test_accordance_input_post():
+    base_test('/accordance_input', get=False)
